bda07294598c343552a8339f70b0026cc0bb43df
RARARA717 <64728152+RARARA717@users.noreply.github.com>
2023-05-19 18:01:50 +0300

    Update ping-pong.py
    last version

--- ping-pong.py
+++ ping-pong.py
@@ -1,12 +1,19 @@
 from pygame import *
 
 
+font.init()
 
 
+font1 = font.Font(None, 35)
+lose = font1.render('Игрок ван проиграл', True, (255,0,0))
+lose2 = font1.render('Игрок ту проиграл', True, (255,0,0))
 
 
 back = transform.scale(image.load('green.jpg'), (600,400))
 window = display.set_mode((600,400))
+
+speed_x = 3
+speed_y = 3
 
 
 speed = 5
@@ -41,22 +48,45 @@ class Hero(GameSprite):
 img_ball = 'ball.png'
 img_line = 'line.png'
 
-line_r = Hero(img_line, 5, 200, 100,100, 5)
-line_l = Hero(img_line, 500, 200, 100, 100 ,5)
+line_r = Hero(img_line, 5, 200, 50,100, 5)
+line_l = Hero(img_line, 550, 200, 50, 100 ,5)
 ball = GameSprite(img_ball, 300,200, 40,40,5)
 
 
+finish = False
 
+game = True
 
-game = False
-
-while not game:
+while game:
+    window.blit(back, (0,0))
     for e in event.get():
         if e.type == QUIT:
-            game = True
+            game = False
+    
+    
+    if finish != True:
+        ball.rect.x +=speed_x
+        ball.rect.y += speed_y
+
+    if ball.rect.y > 400 - 80 or ball.rect.y < 0:
+        speed_y *= -1
+    
+    if sprite.collide_rect(line_l, ball) or sprite.collide_rect(line_r, ball):
+        speed_x *= -1.2
+        speed_y *=1
+
+    
+
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose,(200,200))
+    if ball.rect.x > 600 - 40:
+        finish = True
+        window.blit(lose2,(200,200))
 
 
-    window.blit(back, (0,0))
+
+
     line_r.update_l()
     line_l.update_r()
     ball.update()
